bf86f4bcac1b7e7dfbcb61610b260d36685f4b96
Ashish Kumar <15678ashish@gmail.com>
2023-07-11 22:26:38 +0530

    not changed muxh

--- lyriks_starter_code/lyriks_starter_code/src/pages/Discover.jsx
+++ lyriks_starter_code/lyriks_starter_code/src/pages/Discover.jsx
@@ -1,3 +1,9 @@
-const Discover = () => <div>Discover</div>;
-
+import { Error, Loader, SongCard } from "../components";
+const Discover = () => {
+  return (
+    <div className="flex flex-col">
+      <div className=""></div>
+    </div>
+  );
+};
 export default Discover;
